2298a94e9b816798880dc2957816b829acadb94c
griff22 <118167947+griff22@users.noreply.github.com>
2023-01-09 11:07:13 +0000

    Update 2005-6.py
    090123

--- 2005-6.py
+++ 2005-6.py
@@ -6,13 +6,53 @@ import os
 # create database
 conn = sqlite3.connect ('C:/Users/Surface/Documents/PROGRAMMING/COURSEWORK/flights0506.db')
 # 
-# create tables base data
-2005 = pd.read_csv('C:/Users/Surface/Documents/PROGRAMMING/COURSEWORK/2005.csv')
-2006 = pd.read_csv('C:/Users/Surface/Documents/PROGRAMMING/COURSEWORK/2006.csv')
-2005.to_sql('2005', con = conn, index = False)
-2006.to_sql('2006', con = conn, index = False)
+# create tables supp info
+airports = pd.read_csv('C:/Users/Surface/Documents/PROGRAMMING/COURSEWORK/airports.csv')
+carriers = pd.read_csv('C:/Users/Surface/Documents/PROGRAMMING/COURSEWORK/carriers.csv')
+planes = pd.read_csv('C:/Users/Surface/Documents/PROGRAMMING/COURSEWORK/plane-data.csv')
+airports.to_sql('airports', con = conn, index = False)
+carriers.to_sql('carriers', con = conn, index = False)
+planes.to_sql('planes', con = conn, index = False)
 #
-# create table master combined
+# create table main data ontime
 c = conn.cursor()
 c.execute('''
-CREATE TABLE combo (
+CREATE TABLE ontime (
+  Year int,
+  Month int,
+  DayofMonth int,
+  DayOfWeek int,
+  DepTime  int,
+  CRSDepTime int,
+  ArrTime int,
+  CRSArrTime int,
+  UniqueCarrier varchar(5),
+  FlightNum int,
+  TailNum varchar(8),
+  ActualElapsedTime int,
+  CRSElapsedTime int,
+  AirTime int,
+  ArrDelay int,
+  DepDelay int,
+  Origin varchar(3),
+  Dest varchar(3),
+  Distance int,
+  TaxiIn int,
+  TaxiOut int,
+  Cancelled int,
+  CancellationCode varchar(1),
+  Diverted varchar(1),
+  CarrierDelay int,
+  WeatherDelay int,
+  NASDelay int,
+  SecurityDelay int,
+  LateAircraftDelay int
+)
+''')
+conn.commit()
+for year in range(2005, 2006):
+    ontime = pd.read_csv('C:/Users/Surface/Documents/PROGRAMMING/COURSEWORK/'+str(year)+'.csv')
+    ontime.to_sql('ontime', con = conn, if_exists = 'append', index = False)
+conn.commit()
+#
+# query 1. When is best time of day/ day of week/ time of year to minimise delays?
