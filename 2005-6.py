de55948f56e981de67399549fdbb858e44b8ab0d
griff22 <118167947+griff22@users.noreply.github.com>
2023-02-26 12:07:50 +0000

    Update 2005-6.py
    260223

--- 2005-6.py
+++ 2005-6.py
@@ -145,13 +145,19 @@ nx.draw_circular(digraph, width=list(df[:10]['Weight'] * 0.001), with_labels=Tru
 #
 #
 # -------------------------
-# QUERY 4. Cascading delays from one airport to another.
+# QUERY 4. Are there cascading delays from one airport to another?
 query = '''
 WITH origin AS (SELECT COUNT(*) count_origin, AVG(DepDelay) avg_delay_origin, Origin airport FROM flights WHERE Cancelled=0 AND DepDelay>=0 GROUP BY Origin ORDER BY AVG(DepDelay) DESC NULLS LAST),
 destination AS(SELECT COUNT(*) count_dest, AVG(DepDelay) avg_delay_dest, Dest airport FROM flights WHERE Cancelled=0 AND DepDelay>=0 GROUP BY Dest ORDER BY AVG(DepDelay) DESC NULLS LAST)
 SELECT * FROM origin JOIN destination ON origin.airport = destination.airport;'''
 cur.execute(query)
 df = pd.DataFrame(cur.fetchall(), columns=['count_origin', 'avg_delay_dest', 'airport', 'count_dest', 'avg_delay_origin', '_'])
+# plot & linefit
+x, y = df['avg_delay_dest'], df['avg_delay_origin']
+m, c = np.polyfit(x, y, deg=1)
+line = m * x + c
+print(f'gradient m: ~{round(m,2)}, intercept c: ~{round(c,2)}')
+# answer is intercept 13.5, gradient 0.34
 fig, ax = plt.subplots(figsize=(10, 10))
 plt.xlim([0, 100])
 plt.ylim([0, 100])
@@ -161,8 +167,9 @@ plt.xlabel("to")
 plt.ylabel("from")
 for i, txt in enumerate(df['airport']):
     ax.annotate(txt, (df['avg_delay_dest'][i], df['avg_delay_origin'][i]))
-import numpy as np
-fit = np.polyfit(df['avg_delay_dest'], df['avg_delay_origin'], deg=1)
-line = fit[0]*df['avg_delay_dest'] + fit[1]
-# polyfit not working? need line plot and equation.
+# answer is yes, there are cascading failures with 34% of the original delay cascading into its next flight and 66% of the delay caught up.
+#
+#
+# -----------------------
+# QUERY 5. MODELLING
 #
