f888769fd5dd55a5129db752d31d1c175e8237a0
griff22 <118167947+griff22@users.noreply.github.com>
2023-03-12 19:51:36 +0000

    Update coursework.r

--- coursework.r
+++ coursework.r
@@ -14,9 +14,12 @@ conn <- dbConnect(RSQLite::SQLite(), 'flights.db')
 # check all there
 dbListTables(conn)
 dbListFields(conn, 'Flights')
+# 
 # how count?
 # -------------------------------------------
 # QUERY 1
 # Average delay per month query
 month <- dbGetQuery(conn, 'SELECT month, AVG(DepDelay) FROM flights WHERE Cancelled=0 AND DepDelay >=0 GROUP BY month')
-# Average delay per month plot
+# answer is April
+#
+# Average delay per month plot?
