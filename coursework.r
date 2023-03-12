6c902d7cb5b9c2e7d02a24897a8d6bc8491b4afd
griff22 <118167947+griff22@users.noreply.github.com>
2023-03-12 10:38:58 +0000

    Update coursework.r
    120323

--- coursework.r
+++ coursework.r
@@ -1,1 +1,14 @@
-
+# preparation
+install.packages('tidyverse')
+library('tidyverse')
+install.packages('RSQLite')
+library('RSQLite')
+install.packages('DBI')
+library('DBI')
+#
+# connect to DB already set up
+conn <- dbConnect(RSQLite::SQLite(), 'flights.db')
+# check all there
+dbListTables(conn)
+dbListFields(conn, 'Flights')
+# how count?
