52ccba71d7ca02c73bfcce6ca3775dc34e7e3382
griff22 <118167947+griff22@users.noreply.github.com>
2023-03-13 13:11:58 +0000

    Update coursework.r

--- coursework.r
+++ coursework.r
@@ -11,11 +11,13 @@ library('ggplot2')
 # connect to DB already set up
 setwd('c:/Users/Surface/Documents/PROGRAMMING/COURSEWORK')
 conn <- dbConnect(RSQLite::SQLite(), 'flights.db')
+#
 # check all there
+dbGetQuery(conn, 'SELECT * FROM flights')
+# answer 14.3m flights
 dbListTables(conn)
 dbListFields(conn, 'Flights')
 # 
-# how count?
 # -------------------------------------------
 # QUERY 1
 # Average delay per month query
@@ -32,4 +34,12 @@ day <- dbGetQuery(conn, 'SELECT DayOfWeek, AVG(DepDelay) FROM flights WHERE Canc
 hour <- dbGetQuery(conn, 'SELECT SUBSTRING(SUBSTRING('00000' || DepTime, -6, 6), 0, 3), AVG(DepDelay) FROM flights WHERE Cancelled=0 AND DepDelay >=0 AND Month=4 AND DayOfWeek=2 GROUP BY SUBSTRING(SUBSTRING('00000' || DepTime, -6, 6), 0, 3)')
 # doesn't like it?
 #
-# QUERY 2
+# QUERY 2. Do older plane suffer more delays?
+age <- dbGetQuery(conn, WITH temp_query AS (SELECT (flights."Year" - "plane-data".Year) AgeAtDep, * FROM flights JOIN "plane-data" ON flights.TailNum = "plane-data".TailNum WHERE "plane-data".Year <> 'None')
+'SELECT AgeAtDep, AVG(DepDelay) FROM temp_query WHERE Cancelled=0 AND DepDelay>=0 AND AgeAtDep NOT IN (-2, -1, 2005, 2006) GROUP BY AgeAtDep')
+# not working?
+# Justify outliers (-1, -2, 2005, 2006)
+# shows only 2 planes account for outliers. how show?
+avg_delay_ageatdep = cur.fetchall()
+avg_delay_ageatdep = {k: v for k,v in avg_delay_ageatdep}
+# 
