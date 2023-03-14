022468cb949e676750995e72dbcbc2b0d8067142
griff22 <118167947+griff22@users.noreply.github.com>
2023-03-14 11:38:35 +0000

    Update coursework.r

--- coursework.r
+++ coursework.r
@@ -26,22 +26,25 @@ str(bestmonth) # answer is $ Month        : int  1 2 3 4 5 6 7 8 9 10 ...$ AVG(D
 # bestmonth <- transform(bestmonth, MonthAbb = month.abb[Month])
 # answer is April
 #
-# Average delay per month plot?
+# plot?
 #
 # Average delay per day of week
-day <- dbGetQuery(conn, 'SELECT DayOfWeek, AVG(DepDelay) FROM flights WHERE Cancelled=0 AND DepDelay >=0 AND Month=4 GROUP BY DayOfWeek')
+bestday <- dbGetQuery(conn, 'SELECT DayOfWeek, AVG(DepDelay) FROM flights WHERE Cancelled=0 AND DepDelay >=0 AND Month=4 GROUP BY DayOfWeek')
 # answer is Tuesday
+#
 # plot?
+#
 # Average delay per hour of day
-hour <- dbGetQuery(conn, 'SELECT SUBSTRING(SUBSTRING('00000' || DepTime, -6, 6), 0, 3), AVG(DepDelay) FROM flights WHERE Cancelled=0 AND DepDelay >=0 AND Month=4 AND DayOfWeek=2 GROUP BY SUBSTRING(SUBSTRING('00000' || DepTime, -6, 6), 0, 3)')
+besthour <- dbGetQuery(conn, 'SELECT SUBSTRING(SUBSTRING('00000' || DepTime, -6, 6), 0, 3), AVG(DepDelay) FROM flights WHERE Cancelled=0 AND DepDelay >=0 AND Month=4 AND DayOfWeek=2 GROUP BY SUBSTRING(SUBSTRING('00000' || DepTime, -6, 6), 0, 3)')
 # doesn't like it?
 #
+# -------------------------------------------
 # QUERY 2. Do older plane suffer more delays?
 age <- dbGetQuery(conn, WITH temp_query AS (SELECT (flights."Year" - "plane-data".Year) AgeAtDep, * FROM flights JOIN "plane-data" ON flights.TailNum = "plane-data".TailNum WHERE "plane-data".Year <> 'None')
 'SELECT AgeAtDep, AVG(DepDelay) FROM temp_query WHERE Cancelled=0 AND DepDelay>=0 AND AgeAtDep NOT IN (-2, -1, 2005, 2006) GROUP BY AgeAtDep')
 # not working?
 # Justify outliers (-1, -2, 2005, 2006)
-# shows only 2 planes account for outliers. how show?
+# shows only 2 planes account for outliers.
 # avg_delay_ageatdep = cur.fetchall()
 # avg_delay_ageatdep = {k: v for k,v in avg_delay_ageatdep}
 # 
